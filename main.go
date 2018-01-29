6668ee00dacdfbec71f1f148370e4d1548439f1b
parkpoomd <parkpoom.d@gmail.com>
2018-01-29 14:31:52 +0700

    main config port

--- main.go
+++ main.go
@@ -3,14 +3,25 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"go-learn-rest-api/handlers"
 )
 
 // How to try it: go run main.go
+// How to test configuration env PORT:
+// first: export PORT=8000 && go install go-learn-rest-api && ../../bin/go-learn-rest-api
+// then: export PORT=8000 && ../../bin/go-learn-rest-api
 func main() {
 	log.Print("Starting the service...")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("Port is not set.")
+	}
+
 	router := handlers.Router()
-	log.Print("The service is ready to listen and serve.")
-	log.Fatal(http.ListenAndServe(":8000", router))
+
+	log.Print("The service is ready to listen and serve PORT: " + port)
+	log.Fatal(http.ListenAndServe(":" + port, router))
 }
